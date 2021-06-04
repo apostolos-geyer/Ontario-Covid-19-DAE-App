e88b1f7a63250c33f6b32095250f08cfc37afb2f
pauljamesgeyer <79337131+pauljamesgeyer@users.noreply.github.com>
2021-06-03 22:36:39 -0400

    updated welcome page

--- src/main/java/jginfosci/covid19/dae/visualEnv/WelcomePage.java
+++ src/main/java/jginfosci/covid19/dae/visualEnv/WelcomePage.java
@@ -3,6 +3,8 @@ package jginfosci.covid19.dae.visualEnv;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.Box;
+import javax.swing.border.BevelBorder;
 import jginfosci.covid19.dae.DateAndTime;
 /**
  *
@@ -10,58 +12,67 @@ import jginfosci.covid19.dae.DateAndTime;
  */
 public final class WelcomePage extends JFrame implements ActionListener{
     private final String JG_LOGO_PATH = "visuals/logo.png";
-    private JPanel titlePanel,loadpan,datepan;
-    private JLabel logo,date,welcomeMessage;
-    private JButton load;
-    private static int welcomelen;
-    
+    private final Color JG_RED = new Color(0xA21515);
+    private JLabel DATE = new JLabel(DateAndTime.dispDate());
+    private JPanel parentPanel, titlePanel,loadPanel,datePanel;
+    private JLabel logo,welcomeMessage;
+    private JButton loadButton;
+    private Container content;
+    private static int welcomeLength;
     
     @SuppressWarnings("Unused")
     void initComponents(){
-    logo = new JLabel(new ImageIcon(JG_LOGO_PATH));
-    welcomeMessage = new JLabel("COVID-19 DATA DASHBOARD");
-    
-    welcomeMessage.setFont(new Font("Cambria",Font.ITALIC,40));
-    welcomeMessage.setForeground(new Color(0xA21515));
-    welcomelen=welcomeMessage.getWidth();
-    System.out.println(welcomeMessage.getWidth());
-    
-    date = new JLabel(DateAndTime.dispDate());
-    date.setFont(new Font("Cambria", Font.BOLD,18));
-    welcomeMessage.setForeground(new Color(0xA21515));
-   
+        
+        //Setting the minimum size of the JFrame
+        setSize(new Dimension(1000,700));
+        setMinimumSize(new Dimension(1000,700));
+        
+        //Setting the layout of content pane and adding a top level JPanel
+        Container c = getContentPane();
+        c.setLayout(new BorderLayout());
+        
+        parentPanel = new JPanel(new BorderLayout());
+        parentPanel.setBackground(JG_RED);
+        c.add(parentPanel, BorderLayout.CENTER);
+                
+            titlePanel = new JPanel();
+            titlePanel.setPreferredSize(new Dimension(1000, 200));
+            titlePanel.setBackground(Color.WHITE);
+            BoxLayout bl = new BoxLayout(titlePanel, BoxLayout.X_AXIS);
+            titlePanel.setLayout(bl);
+            titlePanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
 
-    load = new JButton("LOAD");
-    load.setFont(new Font("Cambria",Font.BOLD,18));
-    load.setPreferredSize(new Dimension(100,50));
-    
-    titlePanel = new JPanel();
-    titlePanel.setBackground(new Color(0xFFFFFF));
-    titlePanel.setPreferredSize(new Dimension(200,200));
-    titlePanel.add(logo);
-    titlePanel.add(welcomeMessage);
-    titlePanel.add(date);
-    
-    loadpan = new JPanel();
-    loadpan.setBackground(new Color (0xA21515));
-    loadpan.setPreferredSize(new Dimension(200,100));
-    loadpan.add(load);
-    
-    datepan = new JPanel();
-    datepan.setBackground(new Color (0xA21515));
-    datepan.setPreferredSize(new Dimension(200,100));
-    
+                logo = new JLabel(new ImageIcon(JG_LOGO_PATH));
+                titlePanel.add(logo);
+                titlePanel.add(Box.createRigidArea(new Dimension(100,0)));
+        
+                welcomeMessage = new JLabel("COVID-19 DATA DASHBOARD  ");
+                welcomeMessage.setFont(new Font("Cambria", Font.ITALIC, 40));
+                welcomeMessage.setForeground(new Color(0xA21515));
+                welcomeLength = welcomeMessage.getWidth();
+                DATE.setFont(new Font("Cambria", Font.PLAIN, 35)); 
+                Box welcMessages = Box.createVerticalBox();
+                welcMessages.add(welcomeMessage);
+                welcMessages.add(DATE);
+        
+            titlePanel.add(welcMessages);
+            
+            loadPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 250, 250));
+            loadPanel.setPreferredSize(new Dimension(1000, 500));
+            loadPanel.setBackground(JG_RED);
 
-    //frame
-    this.setSize(1000,700);
-    setLayout(new BorderLayout());
-    getContentPane().setBackground(new Color(0xA21515));
-    //this.add(logo);
-    //this.add(welcomeMessage);
-    add(titlePanel,BorderLayout.NORTH);
-    add(loadpan,BorderLayout.SOUTH);
-    this.setMinimumSize(new Dimension(1000,700));
-    //this.add(load);
+                loadButton = new JButton("LOAD");
+                loadButton.setFont(new Font("Cambria", 0, 25));
+                loadButton.setBackground(Color.WHITE);
+                loadButton.setForeground(JG_RED);
+
+            loadPanel.add(loadButton);
+
+            
+        parentPanel.add(titlePanel, BorderLayout.NORTH);
+        parentPanel.add(loadPanel, BorderLayout.SOUTH);
+        pack();
+
     }
     
     public WelcomePage(){
