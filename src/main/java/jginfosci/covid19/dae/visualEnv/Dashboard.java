b8816065e5bcfdf767aa8a15da827dce263baceb
pauljamesgeyer <79337131+pauljamesgeyer@users.noreply.github.com>
2021-06-07 11:58:25 -0400

    test

---
+++ src/main/java/jginfosci/covid19/dae/visualEnv/Dashboard.java
@@ -0,0 +1,15 @@
+package jginfosci.covid19.dae.visualEnv;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author paulgeyer
+ */
+public class Dashboard {
+    
+}
